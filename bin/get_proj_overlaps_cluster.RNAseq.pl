6e8ff5a1b4b1f6a539171dde5512cc2a49d312d6
David Gonzalez <david.gonzalez@crg.es>
2012-04-11 20:25:51 +0000

    Get the overlaps from the BAM file in fastq

--- bin/get_proj_overlaps_cluster.RNAseq.pl
+++ bin/get_proj_overlaps_cluster.RNAseq.pl
@@ -35,15 +35,18 @@ BEGIN {
 # This script should run overlap on a set reads and exon projections
 
 # Load some modules
-use RNAseq_pipeline3 ('get_fh');
-use RNAseq_pipeline_settings3 ('read_config_file','read_file_list',
-			       'get_feature_overlap_sub');
+use RNAseq_pipeline3 ('get_fh','get_annotation_from_gtf');
+use RNAseq_pipeline_settings3 ('read_config_file','read_file_list');
 use Getopt::Long;
+use Bio::DB::Sam;
+
+
 
 # Declare some variables
 my $threshold=1;
 my $tmpdir;
 my $clusterdir;
+my $samdir;
 my $genomedir;
 my $exondir;
 my $splitdir;
@@ -63,6 +66,7 @@ GetOptions('threshold|t=i' => \$threshold,
 my %options=%{read_config_file()};
 $tmpdir=$options{'LOCALDIR'};
 $genomedir=$options{'PROJECT'}.'/'.$options{'GENOMEDIR'};
+$samdir=$options{'PROJECT'}.'/'.$options{'SAMDIR'};
 $exondir=$options{'EXONDIR'};
 $splitdir=$options{'SPLITMAPDIR'};
 $junctionsdir=$options{'JUNCTIONSDIR'};
@@ -77,69 +81,148 @@ unless($options{'CLUSTER'}) {
     $parallel='default';
 }
 
-unless ($paralleltmp) {
-    $paralleltmp=$options{'PROJECT'}.'/work';
-}
+my $annotation=$genomedir.'/'.$prefix.'.proj.gtf';
+print STDERR $annotation,"\n";
 
-# Get the required sub
-*get_feature_overlap=get_feature_overlap_sub($parallel,
-					     $paralleltmp,
-					     $bindir,
-					     $options{'CLUSTER'});
 
 # First get the files we are going to analyze
 my %files=%{read_file_list($file_list)};
 
-# Get for each lane the files corresponding to the genome mapping and the
-# junctions mapping
-my %lane_files=%{get_lane_files(\%files,
-				$genomedir)};
-
-my $annotation=$genomedir.'/'.$prefix.'.proj.gtf';
+# Get for each lane the BAM files
+my %lane_files=%{get_lane_files(\%files)};
 
 my %coverage;
 
+# Extract the genes from the annotation
+my %genes=%{get_annotation_from_gtf($annotation)};
+
 foreach my $pair (keys %lane_files) {
-    # Find the coverage granted by genome mapping;
-    my $outfile=$lane_files{$pair};
-    $outfile=~s/.gz$/.proj/;
-    get_feature_overlap($lane_files{$pair},
-			$annotation,
-			$stranded,
-			$outfile,
-			$tmpdir);
+    my $bamfile=$samdir.'/'.$pair.'.merged.bam';
+    my $type=$lane_files{$pair};
+    print STDERR "$bamfile\n";
+    my $outfile=$genomedir.'/'.$pair.'.'.$type.'.unique.gtf.proj.overlap.total';
+	
+    if (-r $outfile) {
+	print STDERR $outfile,"\tis present. Skipping\n";
+	next;
+    }
+	
+    # Get the necessary filehandles
+    my $sam = Bio::DB::Sam->new(-bam  =>$bamfile,
+				-autoindex => 1);
+    my $outfh=get_fh($outfile,1);
+	
+    # Process the genes
+    foreach my $gene (keys %genes) {
+	my @exons=$genes{$gene}{'gene'}->exons();
+	my $chr=$genes{$gene}{'chr'};
+	my %gene_hits;
+	# Get the exon information
+	foreach my $exon (@exons) {
+	    my $feat_hits=process_feature($exon,
+					  $chr,
+					  $sam,
+					  $gene);
+	    
+	    $gene_hits{'exon'}{$exon}=$feat_hits;
+	    $gene_hits{'gene'}+=$feat_hits->[2];
+	}
+	
+	# Print the output
+	foreach my $exon (@exons) {
+	    my $exon_id=join("\t",
+			     $chr,
+			     $exon->start(),
+			     $exon->end(),
+			     $exon->strand());
+	    # only print if there are actually reads in the gene
+	    if ($gene_hits{'exon'}{$exon}->[2]) {
+		my $length=$exon->end() - $exon->start() + 1;
+		my $frac=sprintf "%.2f",$gene_hits{'exon'}{$exon}->[2]/$length;
+		print $outfh join("\t",
+				  $gene,
+				  $gene_hits{'exon'}{$exon}->[2],
+				  $length,
+				  'exon',
+				  $frac),"\n";
+	    }
+	}
+	
+    }
+    close($outfh);
 }
 
 exit;
 
 sub get_lane_files {
     my $files=shift;
-    my $dir=shift;
 
     my %lane_files;
 
     foreach my $file (keys %{$files}) {
 	my $pair=$files->{$file}->[0];
 	my $lane=$files->{$file}->[1];
-	$lane_files{$pair}{$lane}=1;
-    }
-
-    # Determine if we are looking at paired or single reads
-    foreach my $pair (keys %lane_files) {
-	my @lanes=keys %{$lane_files{$pair}};
-	if (@lanes == 1) {
-	    # single files
-	    print STDERR "$pair set identified as single reads\n";
-	    $lane_files{$pair}=$dir.'/'.$lanes[0].'.single.unique.gtf.gz';
-	} elsif (@lanes == 2) {
-	    # paired files
-	    print STDERR "$pair set identified as paired reads\n";
-	    $lane_files{$pair}=$dir.'/'.$pair.'.paired.unique.gtf.gz';
+	if ($pair eq $lane) {
+	    $lane_files{$pair}='single';
 	} else {
-	    # This should never happen
-	    die "Apparently there are more than two files grouped\n";
+	    $lane_files{$pair}='paired';
 	}
     }
 
     return(\%lane_files);
+}
+
+sub process_feature {
+    my $feat=shift;
+    my $chr=shift;
+    my $sam=shift;
+    my $gene_id=shift;
+
+    # Make sure the features are in the correct order to avoid errors in the
+    # estimation of minus strand features
+    my ($rstart,$rend)=sort {$a <=> $b} ($feat->start(),
+					 $feat->end());
+    my @alignments = $sam->get_features_by_location(-seq_id => $chr,
+						    -start  => $rstart,
+						    -end    => $rend);
+    
+    my %hits;
+    my $hits=0;
+    my $feat_hits=[0,0,0];
+    for my $al (@alignments) {	
+	my $seqid  = $al->seq_id;
+	
+	# where does the alignment start in the reference sequence
+	my $start  = $al->start;
+	my $end    = $al->end;
+	my $cigar  = $al->cigar_str;
+	my $strand = $al->strand;
+
+	if ($rstart > $start) {
+	    # This removes the possibility of counting the junction
+	    # reads twice if one end is in one interval and the other in
+	    # another
+	    # Skip reads that start outside of the feature
+	    next;
+	} elsif ($cigar=~/(\d+)M(\d+)[NI](\d+)M/o) {
+	    # Skip junction hits
+	    next;
+	} elsif ($rend < $end) {
+	    # Skip reads that end outside of the feature
+	    next;
+	}
+	# where does the alignment start in the query sequence
+	my $query_start  = $al->query->start;
+	my $query_end    = $al->query->end;
+	my $query_length = $al->query->length; # matched length
+		
+	$hits{$strand}++;
+	$hits++;
+    }
+    my $forward=$hits{1} || 0;
+    my $reverse=$hits{-1} || 0;
+    if ($hits) {
+	$feat_hits=[$forward,$reverse,$hits];
+    }
+    return($feat_hits);
 }
