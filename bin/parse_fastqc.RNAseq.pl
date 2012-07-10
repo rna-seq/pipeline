4636efc1c796451be2020057ef605fb693a67482
David Gonzalez <david.gonzalez@crg.es>
2012-07-10 07:08:39 +0000

    Fix bug in finding libraries

--- bin/parse_fastqc.RNAseq.pl
+++ bin/parse_fastqc.RNAseq.pl
@@ -1,8 +1,37 @@
 #!/soft/bin/perl
 # DGK
 
+
+#    GRAPE
+#    Copyright (C) 2011 Centre for Genomic Regulation (CRG)
+#
+#    This file is part of GRAPE.
+#
+#    GRAPE is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GRAPE is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GRAPE.  If not, see <http://www.gnu.org/licenses/>.
+
+#    Author : David Gonzalez, david.gonzalez@crg.eu
+
 use strict;
 use warnings;
+
+# Add the path to the library to be used
+BEGIN {
+    use Cwd 'abs_path';
+    my $libdir=abs_path($0);
+    $libdir=~s/bin\/[^\/]*$/lib/;
+    unshift @INC, "$libdir";
+}
 
 # Objective
 # This script should take the output of fastqc and parse it organizing it into
